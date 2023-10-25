632212439722b4b0d974e9d6520f01a2aa323e3a
storm <stormarasa2@gmail.com>
2023-10-26 01:42:00 +0300

    first test going through

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Links(props){
+    return(
+        <div>
+        <h3>Links</h3>
+        <a href= {props.Links}>Click me</a>
+        </div>
+    )
+}
+export default Links;
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,6 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Storm",
+  city: "Nairobi",
   bio: "I made this!",
   color: "firebrick",
   links: {
